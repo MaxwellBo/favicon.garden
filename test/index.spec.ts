81821297f1aa437ff3b03e423ea9a488b40fcbe7
Max Bo <mjbo@canva.com>
2025-02-24 11:12:31 +1100

    More

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,6 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-import worker from '../src/index';
 
 describe('favicon.garden service', () => {
 	describe('HTML pages', () => {
